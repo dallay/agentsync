7f5721a76270097dd17c3dddaa94225090e11339
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2026-02-01 10:15:17 +0100

    fix: update imports to use node built-ins and improve CSS styles

--- website/docs/astro.config.mjs
+++ website/docs/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+
 import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 import Icons from "unplugin-icons/vite";
 
 // https://astro.build/config
